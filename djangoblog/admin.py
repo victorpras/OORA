20f1ccfea4d3f23e818454bd794f7052e8b76741
Jonathan Hadisuryo <jonathan.hadisuryo@gmail.com>
2012-03-27 14:59:02 -0700

    remove blog

--- djangoblog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from djangoblog.blog.models import Entry
-from djangoblog.blog.admin import EntryAdmin
-
-class AdminSite(admin.AdminSite):
-    pass
-
-site = AdminSite()
-site.register(Entry, EntryAdmin)
