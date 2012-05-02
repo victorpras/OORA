6e57a98d94f6dd25e6e0657b8b592b63ef93ee31
Jonathan Hadisuryo <jonathan.hadisuryo@gmail.com>
2012-05-02 16:20:21 -0700

    phpfogappTwilioAPI

--- oora.phpapp/index.php
+++ oora.phpapp/index.php
@@ -1,39 +1,14 @@
 <?php
-  $con = mysql_connect($server = "mysql-shared-02.phpfog.com", $username = "Custom App-35792", $password = "Asdf1234");
-  if (!$con) {
-    die('Could not connect: ' . mysql_error());
-  }
-  mysql_select_db("oora_phpfogapp_com");
-
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $sql="INSERT INTO Order (RestaurantName, Name, Orders, Status, Comment) VALUES ('$_POST[restaurantName]','$_POST[name]','$_POST[order]'),'$_POST[status]'),'$_POST[comment]')";
-    if (!mysql_query($sql,$con)) {
-      die('Error: ' . mysql_error());
-    }
-    echo "Your order has been placed. Thank you.";
-  }
-  elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $result = mysql_query("SELECT * FROM Order WHERE RestaurantName='//restaurant name'");
-    while($row = mysql_fetch_array($result)) {
-      //echo $row['FirstName'] . " " . $row['LastName'];
-      //echo "<br />";
-      //fetch all information
-    }
-    //need update??
-  }
-
-  mysql_close($con);
+  require "lib/Services/Twilio.php";
+  $phoneNum = $_POST["dataNum"];
+  $orderID = $_POST["dataID"];
+  $restName = $_POST["dataRest"];
+  $AccountSid = "AC5660ef3978e789901ae909cca3b5dec5";
+  $AuthToken = "fd0af4db87137c0cb6b3b050c37bcb93";
+  $client = new Services_Twilio($AccountSid, $AuthToken);
+  $sms = $client->account->sms_messages->create("562-275-8881",$phoneNum,"http://dl.dropbox.com/u/12374742/page.html?restName=".$restName."&orderID=".$orderID);
+  echo "Message has been sent ";
+  echo $phoneNum . " ";
+  echo $orderID . " ";
+  echo $restName;
 ?>
-
-<html>
-<body>
-  <form action="<?php $_PHP_SELF ?>" method="POST">
-
-  Name: <input type="text" name="name" />
-  Restaurant: <input type="text" name="restaurant_name" />
-  Order: <input type="text" name="order" />
-
-  <input type="submit" />
-  </form>
-</body>
-</html>
